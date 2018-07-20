754a8fa8244ad0f78dc1a52287648f1aac0e7432
Otavio Pellicano <otavio.pellicano@gmail.com>
2018-07-20 17:21:02 -0300

    Retirei o coletor da visualização

--- detalheindicador.cpp
+++ detalheindicador.cpp
@@ -7,9 +7,9 @@ DetalheIndicador::DetalheIndicador(QWidget *parent) :
 {
     ui->setupUi(this);
     QStringList header;
-    header << "Prestadora" << "UF" << "Conexão" << "Medições Válidas" << "Coletores Válidos" <<
+    header << "Prestadora" << "UF" << "Conexão" << "Medições Válidas" <<
               "Índice 39" << "Índice 40" << "Índice 41" << "Índice 42";
-    ui->tableWidget_detalhe->setColumnCount(9);
+    ui->tableWidget_detalhe->setColumnCount(8);
     ui->tableWidget_detalhe->setHorizontalHeaderLabels(header);
 
 }
@@ -41,11 +41,10 @@ void DetalheIndicador::atualizarTabelaIndice(const QString& prestadora, const QS
                 ui->tableWidget_detalhe->setItem(linha, 1, new QTableWidgetItem(strCsv[colUF]));
                 ui->tableWidget_detalhe->setItem(linha, 2, new QTableWidgetItem(strCsv[colWanmode]));
                 ui->tableWidget_detalhe->setItem(linha, 3, new QTableWidgetItem(strCsv[colMedVal]));
-                ui->tableWidget_detalhe->setItem(linha, 4, new QTableWidgetItem(strCsv[colColVal]));
-                ui->tableWidget_detalhe->setItem(linha, 5, new QTableWidgetItem(strCsv[colInd_39]));
-                ui->tableWidget_detalhe->setItem(linha, 6, new QTableWidgetItem(strCsv[colInd_40]));
-                ui->tableWidget_detalhe->setItem(linha, 7, new QTableWidgetItem(strCsv[colInd_41]));
-                ui->tableWidget_detalhe->setItem(linha, 8, new QTableWidgetItem(strCsv[colInd_42]));
+                ui->tableWidget_detalhe->setItem(linha, 4, new QTableWidgetItem(strCsv[colInd_39]));
+                ui->tableWidget_detalhe->setItem(linha, 5, new QTableWidgetItem(strCsv[colInd_40]));
+                ui->tableWidget_detalhe->setItem(linha, 6, new QTableWidgetItem(strCsv[colInd_41]));
+                ui->tableWidget_detalhe->setItem(linha, 7, new QTableWidgetItem(strCsv[colInd_42]));
 
                 ++linha;
             }
