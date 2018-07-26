9c15c870726d4a0648397a57dfcc71426fb4dda2
Otavio Pellicano <otavio.pellicano@gmail.com>
2018-07-26 13:44:05 -0300

    Configurações iniciais do dialog

--- regrasdescarte.cpp
+++ regrasdescarte.cpp
@@ -6,9 +6,154 @@ RegrasDescarte::RegrasDescarte(QWidget *parent) :
     ui(new Ui::RegrasDescarte)
 {
     ui->setupUi(this);
+
+    QStringList header;
+    header << "Código" << "Descrição";
+    ui->tableWidget_ativadas->setColumnCount(2);
+    ui->tableWidget_desativadas->setColumnCount(2);
+    ui->tableWidget_ativadas->setHorizontalHeaderLabels(header);
+    ui->tableWidget_desativadas->setHorizontalHeaderLabels(header);
+    ui->tableWidget_ativadas->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    ui->tableWidget_ativadas->setSelectionBehavior(QAbstractItemView::SelectRows);
+    ui->tableWidget_desativadas->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    ui->tableWidget_desativadas->setSelectionBehavior(QAbstractItemView::SelectRows);
+
+    ui->tableWidget_ativadas->horizontalHeader()->setStretchLastSection(true);
+    ui->tableWidget_desativadas->horizontalHeader()->setStretchLastSection(true);
+
+    carregarMapCodigoDescarteDoArquivo();
+    if(mMapCodigoDescricaoAtivo.empty() && mMapCodigoDescricaoInativo.empty())
+    {
+        atualizarTabela(*ui->tableWidget_ativadas, mMapCodigoDescricao);
+    }
+    else
+    {
+        atualizarTabela(*ui->tableWidget_ativadas, mMapCodigoDescricaoAtivo);
+        atualizarTabela(*ui->tableWidget_desativadas, mMapCodigoDescricaoInativo);
+    }
+
 }
 
 RegrasDescarte::~RegrasDescarte()
 {
     delete ui;
+}
+
+void RegrasDescarte::carregarMapCodigoDescarteDoArquivo()
+{
+
+    mMapCodigoDescricaoAtivo.clear();
+    mMapCodigoDescricaoInativo.clear();
+
+    QDir dir;
+    std::ifstream arq((dir.absolutePath() + "/" + mNomeDirRef + "/" + mFileNamePA).toStdString());
+    std::string str;
+    QStringList strCsv;
+
+    if(arq.is_open())
+    {
+        for(size_t i = 0; std::getline(arq,str); ++i)
+        {
+            if(i == 0)
+                continue;
+            strCsv = QString::fromStdString(str).split(";");
+
+            if(strCsv[2] == "1")
+            {
+                mMapCodigoDescricaoAtivo[strCsv[0]] = strCsv[1];
+            }
+            else if(strCsv[2] == "0")
+            {
+                mMapCodigoDescricaoInativo[strCsv[0]] = strCsv[1];
+            }
+        }
+    }
+    else
+    {
+        qDebug() << "nao existe arquivo de codigo de descarte";
+    }
+}
+
+void RegrasDescarte::atualizarTabela(QTableWidget &tabela, const mapQStr &mapAtivDes)
+{
+    tabela.clearContents();
+    size_t linha = 0;
+
+    for(auto itMap = mapAtivDes.begin(); itMap != mapAtivDes.end(); ++itMap)
+    {
+        tabela.setRowCount(linha + 1);
+        tabela.setItem(linha, 0, new QTableWidgetItem(itMap->first));
+        tabela.setItem(linha, 1, new QTableWidgetItem(itMap->second));
+        ++linha;
+    }
+
+}
+
+void RegrasDescarte::carregarMapsCodigoDescarte(const QTableWidget &tabela, mapQStr &mapAtivDes)
+{
+    mapAtivDes.clear();
+    for(int i = 0; i < tabela.rowCount(); ++i)
+    {
+        mapAtivDes[tabela.item(i,0)->text()] = tabela.item(i,1)->text();
+    }
+}
+
+bool RegrasDescarte::salvarRegras(const mapQStr &mapAtivo, const mapQStr &mapInativo)
+{
+    QDir dir;
+    dir.mkdir(mNomeDirRef);
+    dir.cd(mNomeDirRef);
+
+    std::ofstream arq((dir.absolutePath() + "/" + mFileNamePA).toStdString());
+
+    if(arq.is_open())
+    {
+        arq << "Codigo;Decricao;ativo" << std::endl;
+
+
+        for(auto itMap = mapAtivo.begin(); itMap != mapAtivo.end(); ++itMap)
+        {
+            arq << itMap->first.toStdString() << ";" << itMap->second.toStdString() << ";1" << std::endl;
+        }
+
+        for(auto itMap = mapInativo.begin(); itMap != mapInativo.end(); ++itMap)
+        {
+            arq << itMap->first.toStdString() << ";" << itMap->second.toStdString() << ";0" << std::endl;
+        }
+    }
+    else
+    {
+        qDebug() << "Impossivel salvar Regras!";
+        return false;
+    }
+
+    return true;
+
+
+
+}
+
+mapQStr RegrasDescarte::mapCodigoDescricaoInativo() const
+{
+    return mMapCodigoDescricaoInativo;
+}
+
+mapQStr RegrasDescarte::mapCodigoDescricaoAtivo() const
+{
+    return mMapCodigoDescricaoAtivo;
+}
+
+void RegrasDescarte::on_pushButton_aplicar_clicked()
+{
+    carregarMapsCodigoDescarte(*ui->tableWidget_ativadas, mMapCodigoDescricaoAtivo);
+    carregarMapsCodigoDescarte(*ui->tableWidget_desativadas, mMapCodigoDescricaoInativo);
+
+    if(salvarRegras(mMapCodigoDescricaoAtivo, mMapCodigoDescricaoInativo))
+    {
+        QMessageBox::information(this, tr("Regras de Descarte"), tr("Regras de Descarte aplicadas!"), QMessageBox::Ok);
+    }
+    else
+    {
+        QMessageBox::critical(this, tr("Regras de Descarte"), tr("Erro ao aplicar as Regras de Descarte!\nRegras não aplicadas!"), QMessageBox::Ok);
+    }
 }
--- regrasdescarte.h
+++ regrasdescarte.h
@@ -2,6 +2,15 @@
 #define REGRASDESCARTE_H
 
 #include <QDialog>
+#include <QStringList>
+#include <map>
+#include <QDir>
+#include <fstream>
+#include <QDebug>
+#include <QTableWidget>
+#include <QMessageBox>
+
+typedef std::map<QString, QString> mapQStr;
 
 namespace Ui {
 class RegrasDescarte;
@@ -15,8 +24,72 @@ public:
     explicit RegrasDescarte(QWidget *parent = 0);
     ~RegrasDescarte();
 
+    mapQStr mapCodigoDescricaoAtivo() const;
+
+    mapQStr mapCodigoDescricaoInativo() const;
+
+private slots:
+    void on_pushButton_aplicar_clicked();
+
+private:
+    void carregarMapCodigoDescarteDoArquivo();
+    void atualizarTabela(QTableWidget& tabela, const mapQStr& mapAtivDes);
+    void carregarMapsCodigoDescarte(const QTableWidget& tabela, mapQStr& mapAtivDes);
+    bool salvarRegras(const mapQStr& mapAtivo, const mapQStr& mapInativo);
+
 private:
     Ui::RegrasDescarte *ui;
+
+    const QString mFileNamePA = "rd_smp.csv";
+    const QString mNomeDirRef = "data_gismp";
+
+    mapQStr mMapCodigoDescricaoAtivo;
+    mapQStr mMapCodigoDescricaoInativo;
+
+    mapQStr mMapCodigoDescricao = {
+        {"00","Validação dos campos esperados"},
+        {"01","Duplicidades"},
+        {"02","Medição com falha - Down ou Up"},
+        {"03","Medição com falha - Latência"},
+        {"04","Medição com falha - Disponibilidade"},
+        {"05","Medição com falha - Jitter"},
+        {"06","PMT"},
+        {"07","Alteração de endereço"},
+        {"08","Alteração de plano"},
+        {"09","Limitação por franquia"},
+        {"10","Suspensão de Serviço"},
+        {"11","Cancelamento de Plano"},
+        {"12","ISP com menos de 180 dias de operação"},
+        {"14","Paridade Download e Upload"},
+        {"15","Quarentena"},
+        {"16","Ping back divergente"},
+        {"17","Stolen/changed probe"},
+        {"18","Medições de voluntário/assinante não validado ou não encontrado"},
+        {"19","Exceção de Infraestrutura"},
+        {"20","3 medições/dia - 3 medições válidas"},
+        {"21","Voluntário não válido"},
+        {"22","Envio de Rawdata fora do prazo"},
+        {"23","Validação dos campos esperados"},
+        {"24","PTT não cadastrado"},
+        {"25","Avaliação da CPU"},
+        {"26","Memória Insuficiente"},
+        {"27","Medições autônomas"},
+        {"28","Avaliação do status"},
+        {"29","Tipo de Conexão"},
+        {"30","Troca de Tecnologia"},
+        {"31","CellId não encontrado"},
+        {"32","Alteração de plano"},
+        {"33","UF sem plano amostral"},
+        {"34","Velocidade contrata acima de 100 Mbps"},
+        {"35","Modelo do hardware/software não compatível"},
+        {"36","Intensidade do sinal"},
+        {"37","CEP Error"},
+        {"38","Município sem Mesorregião usado no PA"},
+        {"39","Invalid CEP/UF/GMT for ISP that send the data in raw"},
+        {"40","Medição sem coletor"}
+    };
+
+
 };
 
 #endif // REGRASDESCARTE_H
